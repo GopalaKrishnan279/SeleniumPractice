d38980f134e7e9b15907e2ee3ec4f0426aa059ce
GopalaKrishnan279 <krishnasuresh279@gmail.com>
2022-07-02 22:15:50 +0530

    I added MyPhoneDay1 Project

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,27 @@
+package org.phone;
+
+public class PhoneInfo {
+
+public void phoneName() {
+	
+System.out.println("Phone Name: iphone");
+}
+
+public void phoneMieiNum() {
+	
+System.out.println("PhoneIMEI :123456789");
+}
+
+
+
+public static void main(String[] args) {
+	PhoneInfo p = new PhoneInfo();
+	
+	p.phoneName();
+	p.phoneMieiNum();
+			
+			
+	
+
+
+}}
